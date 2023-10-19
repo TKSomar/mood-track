392071d082cf68443729d23f423e19b69837004b
TKSomar <tksomar@gmail.com>
2023-10-19 15:21:43 -0500

    add handleSubmit to login form

--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -1,13 +1,29 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import styles from './Login.module.css';
 
 const Login: React.FC = (): ReactElement => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        // Request to API
+
+        console.log("Email:", email);
+        console.log("Password:", password);
+  };
+
     return (
         <div className={styles.loginContainer}>
 
-                <input id="email" className={styles.loginInput} type="email" placeholder='Email' />
+                <input id="email" className={styles.loginInput} type="email" value={email} placeholder='Email' onChange={(e) => setEmail(e.target.value)} />
 
-                <input id="password" className={styles.loginInput} type="password" placeholder='Password' />
+                <input id="password" className={styles.loginInput} type="password" value={password} placeholder='Password' onChange={(e) => setPassword(e.target.value)} />
+
+                <div>
+                    <button type="submit" onClick={handleSubmit}>Login</button>
+                </div>
         </div>
     )
 }
